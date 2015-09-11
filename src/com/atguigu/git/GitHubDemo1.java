c2761358e82155afe750afb61aaee4fb76bb88d6
749848395 <749848395@qq.com>
2015-09-11 22:45:06 +0800

    Update GitHubDemo1.java
    github response 01

--- src/com/atguigu/git/GitHubDemo1.java
+++ src/com/atguigu/git/GitHubDemo1.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class GitHubDemo1 {
 	public static void main(String[] args) {
 		System.out.println("welcome to atguigu,0423同学们好");
+		System.out.println("Git Hub send message to 0423");
 	}
 }
