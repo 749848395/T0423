811a3db371956ec9257b3b8111e63374b674fc73
749848395 <749848395@qq.com>
2015-09-11 23:18:11 +0800

    Update GitHubDemo1.java
    update

--- src/com/atguigu/git/GitHubDemo1.java
+++ src/com/atguigu/git/GitHubDemo1.java
@@ -4,5 +4,6 @@ public class GitHubDemo1 {
 	public static void main(String[] args) {
 		System.out.println("welcome to atguigu,0423同学们好");
 		System.out.println("Git Hub send message to 0423");
+		System.out.println("服务器更新了");
 	}
 }
